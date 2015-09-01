a004dc28840aae8b5358ffdcd2bb40aebcd1cdd0
flipmcf <flipmcf@gmail.com>
2015-09-01 17:14:06 -0400

    smart video works, but requests kaltura info way too much right now.  Needs some kind of caching

--- rfa/kaltura/content/smartvideo.py
+++ rfa/kaltura/content/smartvideo.py
@@ -10,6 +10,7 @@ from Products.ATContentTypes.content.schemata import ATContentTypeSchema
 from Products.ATContentTypes.content.schemata import finalizeATCTSchema
 from Products.Archetypes import atapi
 
+from rfa.kaltura import kutils
 from rfa.kaltura.content import base as KalturaBase
 from rfa.kaltura.interfaces import IKalturaSmartVideo
 from rfa.kaltura.config import PROJECTNAME
@@ -26,6 +27,8 @@ SmartVideoSchema["tags"].widget.visible = {"edit": "invisible"}
 
 finalizeATCTSchema(SmartVideoSchema, moveDiscussion=False)
 
+from zope.interface import Interface
+
 class KalturaSmartVideo(base.ATCTContent, KalturaBase.KalturaContentMixin):
     
     implements(IKalturaSmartVideo)
@@ -33,10 +36,25 @@ class KalturaSmartVideo(base.ATCTContent, KalturaBase.KalturaContentMixin):
     schema = SmartVideoSchema
     
     security = ClassSecurityInfo()
-    
+        
     security.declarePrivate('getDefaultPlayerId')
     def getDefaultPlayerId(self):
         return "20100652" #todo - add to config
-    
 
+    
+    def dynamicKalturaObject(self):
+        
+        catFilter = kutils.makeFilter(catIds=self.categories.keys())
+        vidlist = kutils.getRecent(limit=1, partner_id=self.partnerId, filt=catFilter)
+        
+        try:
+            self._cachedKalturaObject = vidlist[0]
+        except IndexError:
+            self._cachedKalturaObject = None
+    
+        return self._cachedKalturaObject
+        
+    KalturaObject = property(dynamicKalturaObject) 
+        
+        
 atapi.registerType(KalturaSmartVideo, PROJECTNAME)
